e3fd4dd8e985272c353af2c70e3228ce7385c988
Ruslan Bekenev <furyinbox@gmail.com>
2025-02-16 13:32:51 +1100

    Implement basic echo server

--- src/main.zig
+++ src/main.zig
@@ -2,7 +2,28 @@ const std = @import("std");
 
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    std.debug.print("Starting the server...\n", .{});
+
+    const ADDR = "0.0.0.0";
+    const PORT = 1965;
+
+    const address = try std.net.Address.parseIp(ADDR, PORT);
+    var server = std.net.Address.listen(address, .{}) catch |e| {
+        std.debug.print("Error happened during listen ({s}).\n", .{@typeName(@TypeOf(e))});
+        std.process.exit(1);
+    };
+    defer server.deinit();
+
+    // indefinitely listen for new connections
+    while (true) {
+        std.debug.print("Listen for new connection.\n", .{});
+        const connection = try server.accept();
+        var buffer: [1024]u8 = undefined;
+        _ = try connection.stream.read(&buffer);
+        std.debug.print("Input {any}.\n", .{buffer});
+        _ = try connection.stream.write(&buffer);
+        connection.stream.close();
+    }
 }
 
 test "simple test" {
